c4b80c26015c505af9e3358396fa6850c4ec88eb
Yanni <yanni.wang@shopee.com>
2019-10-18 12:26:24 +0800

    no message

---
+++ NotSecureSwizzlingTests/NotSecureSwizzlingTests.swift
@@ -0,0 +1,33 @@
+//
+//  NotSecureSwizzlingTests.swift
+//  NotSecureSwizzlingTests
+//
+//  Created by Yanni Wang on 18/10/19.
+//  Copyright © 2019 Yanni. All rights reserved.
+//
+
+import XCTest
+
+class NotSecureSwizzlingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ SecureSwizzling/SecureSwizzling.h
@@ -0,0 +1,19 @@
+//
+//  SecureSwizzling.h
+//  SecureSwizzling
+//
+//  Created by Yanni Wang on 18/10/19.
+//  Copyright © 2019 Yanni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecureSwizzling.
+FOUNDATION_EXPORT double SecureSwizzlingVersionNumber;
+
+//! Project version string for SecureSwizzling.
+FOUNDATION_EXPORT const unsigned char SecureSwizzlingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecureSwizzling/PublicHeader.h>
+
+
